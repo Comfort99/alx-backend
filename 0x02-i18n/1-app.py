55030603543b5688816b8404c22a951098c5f3f5
Comfort99 <shingangecomfort@gmail.com>
2024-08-28 01:57:01 +0200

    Babel Config

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+""" Basic Flask_Babel """
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+class Config:
+    """ Class that configures Babel
+    language, lacation and timezone """
+    LANGUAGE = ["en", "fr"]
+    BABEL_DEFAULT_LOCATE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+# create flask instance
+# config babel and instantiate babel
+app = Flask(__name__)
+app.config.from_object(Config)
+app.url_map.strict_slashes = False
+babel = Babel(app)
+
+
+@app.route('/')
+def basic_babel() -> str:
+    """ A basic route render function """
+    return render_template('1-index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
